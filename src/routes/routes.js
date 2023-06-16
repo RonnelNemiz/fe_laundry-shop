0e09293695e5a046937056e893481e7929eab2d9
RonnelNemiz <r.nemiz@mlgcl.edu.ph>
2023-06-16 11:44:35 +0800

    Added a route for public fetch of services

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -12,7 +12,7 @@ const routes = [
     component: "modules/public/contact/Contact",
   },
   {
-    path: "/services",
+    path: "show/services",
     component: "modules/public/services/Services",
   },
 
