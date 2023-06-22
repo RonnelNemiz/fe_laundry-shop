80f2b5c5d01f80f0a1ba350d1d4a596330949917
linakaren <linakaren69@gmail.com>
2023-06-23 03:31:27 +0800

    Settings dataTables

--- src/layouts/public/Footer.js
+++ src/layouts/public/Footer.js
@@ -17,45 +17,56 @@ function Footer() {
             <h6 className="quicklink">QUICK LINKS</h6>
             <div className="underline mx-auto"></div>
             <div>
-              <Link to="/" className="haco">Home</Link>
+              <Link to="/" className="haco">
+                Home
+              </Link>
             </div>
             <div>
-              <Link to="about" className="haco">About</Link>
+              <Link to="about" className="haco">
+                About
+              </Link>
             </div>
             <div>
-              <Link to="/contact" className="haco">Contact</Link>
+              <Link to="/contact" className="haco">
+                Contact
+              </Link>
             </div>
             <div>
-              <Link to="/order" className="haco">Order</Link>
+              <Link to="/order" className="haco">
+                Order
+              </Link>
             </div>
           </div>
           <div className="container1 col-md text-center">
             <h6 className="operatinghours">OPERATING HOURS</h6>
             <div className="underline mx-auto"></div>
-            <div className="text-white mb-1" id="haco">Monday to Sunday</div>
-            <div className="text-white mb-1" id="haco">8AM to 12 midnight </div>
-            <div className="text-white mb-1" id="haco">8AM to 12 midnight </div>
+            <div className="text-white mb-1" id="haco">
+              Monday to Sunday
+            </div>
+            <div className="text-white mb-1" id="haco">
+              8AM to 5PM{" "}
+            </div>
           </div>
           <div className="container1 col-md text-center">
             <h6 className="contactinformation">CONTACT INFORMATION</h6>
             <div className="underline mx-auto"></div>
             <div className="text-white mb-1" id="haco">
-              <i className="fa-solid fa-location-dot" ></i>ML Flores St. Central
+              <i className="fa-solid fa-location-dot"></i>ML Flores St. Central
               Poblacion
             </div>
             <div className="text-white mb-1" id="haco">
               <i className="fa-solid fa-phone-volume"></i>+639531093959
             </div>
             <div className="text-white mb-1" id="haco">
-              <i className="fa-solid fa-envelope"></i>laundryshop123@gmail.com
+              <i className="fa-solid fa-envelope"></i>treslabanderas@gmail.com
             </div>
             <div className="text-white mb-1"></div>
           </div>
-          <p className="copyRightStyle" id="haco">© 2023 Laundry Shop Management System. All Rights Reserved.</p>
+          <p className="copyRightStyle" id="haco">
+            © 2023 Laundry Shop Management System. All Rights Reserved.
+          </p>
         </div>
-      
       </div>
-    
     </section>
   );
 }
